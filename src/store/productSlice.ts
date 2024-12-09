4215c765623c6c0622a98330fcb327ddee66273f
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-12-09 11:15:02 +0200

    add productSlice and connect to store, also add server-start-script

---
+++ src/store/productSlice.ts
@@ -0,0 +1,51 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Product {
+  id: number;
+  imageUrl: string;
+  name: string;
+  count: number;
+  size: {
+    width: number;
+    height: number;
+  };
+  weight: string;
+  comments: string[];
+}
+
+interface ProductState {
+  products: Product[];
+}
+
+const initialState: ProductState = {
+  products: [],
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    setProducts(state, action: PayloadAction<Product[]>) {
+      state.products = action.payload;
+    },
+    addProduct(state, action: PayloadAction<Product>) {
+      state.products.push(action.payload);
+    },
+    deleteProduct(state, action: PayloadAction<number>) {
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload
+      );
+    },
+    updateProduct(state, action: PayloadAction<Product>) {
+      const index = state.products.findIndex((p) => p.id === action.payload.id);
+      if (index !== -1) {
+        state.products[index] = action.payload;
+      }
+    },
+  },
+});
+
+export const { setProducts, addProduct, deleteProduct, updateProduct } =
+  productSlice.actions;
+
+export default productSlice.reducer;
